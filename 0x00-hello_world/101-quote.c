9e44e565e2b8b9b31388990b9ab34445fa71837d
ethar <ethar@example.com>
2023-03-16 12:37:35 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	writef(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	fwrite(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
